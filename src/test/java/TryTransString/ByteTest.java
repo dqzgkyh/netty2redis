6d07a9175a3bf6933ab0488a66b0599bc0790bd7
qqq <qqq @email.com>
2019-12-12 21:53:15 +0800

    尝试利用 debugger 进行 debug

--- src/test/java/TryTransString/ByteTest.java
+++ src/test/java/TryTransString/ByteTest.java
@@ -6,18 +6,28 @@ import io.netty.buffer.Unpooled;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
-public class ByteTest {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String example = "This is an example";
-        byte[] bytes = example.getBytes();
 
+
+public class ByteTest {
+    static String example = "This is an example";
+    static byte[] bytes = example.getBytes();
+
+    public static void print1(){
+        Integer int1 = Integer.parseInt("3");
         System.out.println("Text : " + example);
         System.out.println("Text [Byte Format] : " + bytes);
         System.out.println("Text [Byte Format] : " + bytes.toString());
+    }
 
+    public static void print2() throws UnsupportedEncodingException {
         String s = new String(bytes,"UTF-8");
         System.out.println("Text Decryted : " + s);
+    }
 
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        print1();
+
+        print2();
 
     }
 }
